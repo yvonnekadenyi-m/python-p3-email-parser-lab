57e22c4cfd81adbd8f0cfcbab099ea66d077bb2d
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-12-03 16:50:49 +0300

    added

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -7,3 +7,18 @@ class EmailAddressParser:
     def __init__(self, email_addresses):
         self.email_addresses = email_addresses
 
+    def parse(self):
+        #filter_string = re.compile(r'[A-z]\w*\.\w+@\w+\.[a-z]+|[A-z]\w*\@\w+\.[a-z]+')
+        #filtered_emails = filter_string.sub("", self.email_addresses)
+        #devide = re.compile(r'[,\s]+')
+        #emails_list = list(set(devide.split(filtered_emails)))
+        #sorted_emails_list = sorted(emails_list, key=lambda v: v[0])
+        #return sorted_emails_list
+
+        filter_string = re.compile(r'[A-z]\w*\.\w+@\w+\.[a-z]+|[A-z]\w*\@\w+\.[a-z]+')
+        filtered_emails = filter_string.findall(self.email_addresses)
+        unique_emails = sorted(set(filtered_emails))
+        return unique_emails
+
+
+        
